0cc63fe0fdc668199cba3e1b3c7c4e16d18a77d2
haoliang <haoliang0.1.2@gmail.com>
2023-07-22 10:49:08 +0800

    sync with upstream

--- lua/grep.lua
+++ lua/grep.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local fn = require("infra.fn")
 local fs = require("infra.fs")
-local jelly = require("infra.jellyfish")("grep", "debug")
+local jelly = require("infra.jellyfish")("grep")
 local listlib = require("infra.listlib")
 local project = require("infra.project")
 local subprocess = require("infra.subprocess")
